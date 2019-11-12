59b0daff93acba8adf39bc455826ede5f9e1ad35
Creighton Fowler Jr <fowlercr1@students.ums-wright.org>
2019-11-12 08:11:48 -0600

    Started Working

---
+++ Hustle-Bro/Hustle-Bro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hustle-Bro
+//
+//  Created by Fowler, Creighton on 11/12/19.
+//  Copyright © 2019 Fowler, Creighton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
